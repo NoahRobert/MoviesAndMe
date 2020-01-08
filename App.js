dc3d952edd8e9cbd43cbe0e3daa9facc01567002
David EKLO <noah.robert45@outlook.fr>
2020-01-07 23:00:17 -0500

    3. Construisez vos vues avec Flexbox

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Search from './Components/Search'
+
 export default class App extends React.Component {
-  render() {
-    return (
-    <Search/>
-    );
+  render () { return (
+    <Search />
+  )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- Components/Search.js
+++ Components/Search.js
@@ -1,25 +1,31 @@
 import React from 'react'
-import {StyleSheet, View, Button, TextInput, Text} from 'react-native'
+import { StyleSheet, View, Button, TextInput} from 'react-native'
+
 class Search extends React.Component {
-  render(){
-    return (
-      <View>
-      <TextInput style={styles.textinput} placeholder="Titre du film"/>
-      <Text>En plus pour tester</Text>
-      <Button title="Rechercher" onPress={() =>{}}/>
-      </View>
-    );
-  }
+  render() { return(
+    <View style = {styles.main_container}>
+    <TextInput style={styles.textinput} placeholder="Titre du film"/>
+    <Button title="Rechercher" onPress={() => {}} />
+    </View>
+  )
+}
 }
 
-const styles = StyleSheet.create({
+const styles_basic = {
+  main_container: {
+    flex: 1,
+    marginTop: 20
+  },
   textinput: {
     marginLeft: 5,
     marginRight: 5,
-    height: 60,
+    height: 50,
     borderColor: '#000000',
     borderWidth: 1,
     paddingLeft: 5
   }
-})
+}
+
+const styles = StyleSheet.create(styles_basic)
+
 export default Search
